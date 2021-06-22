046289f19457fe4c432a4888280a5f5ff4f14e93
yesung1 <sys09092@gmail.com>
2021-06-22 17:55:48 +0900

    update useState, useRef

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    });
+    const nameInput = useState();
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
-    }
+        const {name , value} = e.target;
+
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
     const onReset = () => {
-        setText('');
+        setInputs({
+            name: '',
+            nickname: '',
+        });
+        nameInput.current.focus();
     }
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
+            <input name="nickname"placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
     )
